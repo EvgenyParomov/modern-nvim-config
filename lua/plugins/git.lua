be64c20ba02bcf1a603165730e8a23d54c00b58f
Evgeny Paromov <evgeny.paromov@gmail.com>
2025-12-14 07:49:28 +0300

    Update git structure

---
+++ lua/plugins/git.lua
@@ -0,0 +1,69 @@
+-- Git integration
+return {
+	-- Gitsigns - git decorations and hunk operations
+	{
+		"lewis6991/gitsigns.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			require("gitsigns").setup({
+				signs = {
+					add = { text = "│" },
+					change = { text = "│" },
+					delete = { text = "_" },
+					topdelete = { text = "‾" },
+					changedelete = { text = "~" },
+				},
+				on_attach = function(bufnr)
+					local gs = package.loaded.gitsigns
+					local opts = { buffer = bufnr }
+
+					vim.keymap.set("n", "]h", gs.next_hunk, opts)
+					vim.keymap.set("n", "[h", gs.prev_hunk, opts)
+					vim.keymap.set("n", "<leader>hs", gs.stage_hunk, opts)
+					vim.keymap.set("n", "<leader>hr", gs.reset_hunk, opts)
+					vim.keymap.set("n", "<leader>hp", gs.preview_hunk, opts)
+					vim.keymap.set("n", "<leader>hb", gs.blame_line, opts)
+				end,
+			})
+		end,
+	},
+
+	-- Vim-fugitive - git commands in Vim
+	{
+		"tpope/vim-fugitive",
+		cmd = { "Git", "G" },
+		keys = {
+			{ "<leader>gs", vim.cmd.Git, desc = "Open Git status" },
+			{ "<leader>gd", ":Gvdiffsplit<CR>", desc = "Git diff split" },
+			{ "<leader>gb", ":Git blame<CR>", desc = "Git blame" },
+			{ "<leader>gc", ":Git commit<CR>", desc = "Git commit" },
+			{ "<leader>gp", ":Git push<CR>", desc = "Git push" },
+			{ "<leader>gl", ":Git log<CR>", desc = "Git log" },
+		},
+	},
+
+	-- Diffview - better diff view
+	{
+		"sindrets/diffview.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		keys = {
+			{ "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Open diff view" },
+			{ "<leader>gdc", "<cmd>DiffviewClose<cr>", desc = "Close diff view" },
+			{ "<leader>gdh", "<cmd>DiffviewFileHistory<cr>", desc = "File history" },
+		},
+		config = function()
+			require("diffview").setup({
+				enhanced_diff_hl = true,
+				use_icons = true,
+				file_panel = {
+					position = "left",
+					width = 35,
+				},
+				file_history_panel = {
+					position = "bottom",
+					height = 16,
+				},
+			})
+		end,
+	},
+}
---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,41 @@
+-- Fuzzy finder
+return {
+	"nvim-telescope/telescope.nvim",
+	branch = "0.1.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	},
+	keys = {
+		{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
+		{ "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
+		{ "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Find buffers" },
+		{ "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help tags" },
+		{ "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent files" },
+		{ "<leader>fs", "<cmd>Telescope lsp_document_symbols<cr>", desc = "Document symbols" },
+		{ "<leader>fd", "<cmd>Telescope diagnostics<cr>", desc = "Diagnostics" },
+		{ "<leader>gc", "<cmd>Telescope git_commits<cr>", desc = "Git commits" },
+		{ "<leader>gb", "<cmd>Telescope git_branches<cr>", desc = "Git branches" },
+	},
+	config = function()
+		local telescope = require("telescope")
+		telescope.setup({
+			defaults = {
+				path_display = { "truncate" },
+				mappings = {
+					i = {
+						["<C-k>"] = require("telescope.actions").move_selection_previous,
+						["<C-j>"] = require("telescope.actions").move_selection_next,
+					},
+				},
+			},
+			pickers = {
+				find_files = {
+					hidden = true,
+					file_ignore_patterns = { "node_modules", ".git/", "dist/", "build/" },
+				},
+			},
+		})
+		telescope.load_extension("fzf")
+	end,
+}
